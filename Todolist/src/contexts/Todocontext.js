e51f20c8c2919ffd03b6073e699ae2bb6459be43
LABHESH PRAJAPATI <labheshprajapatilucky@gmail.com>
2024-07-17 12:25:07 +0530

    Todo list add

---
+++ Todolist/src/contexts/Todocontext.js
@@ -0,0 +1,20 @@
+import { createContext, useContext } from "react";
+export const Todocontext = createContext({
+  todos: [
+    {
+      id: 1,
+      todo: "todo msg",
+      completed: false,
+    },
+  ],
+  addTodo: (todo) => {},
+  updateTodo: (id, todo) => {},
+  deleteTodo: (id) => {},
+  toggleCompleted: (id) => {},
+});
+
+export const useTodo = () => {
+  return useContext(Todocontext);
+};
+
+export const Todoprovider = Todocontext.Provider;
