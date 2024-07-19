f945fcf49685596acd9285786ac2e987aa9e0f7b
LABHESH PRAJAPATI <labheshprajapatilucky@gmail.com>
2024-07-19 11:39:02 +0530

    add ReduxToolkitTodo

---
+++ ReduxToolkitTodo/src/App.jsx
@@ -0,0 +1,18 @@
+
+import Todos from './components/Todos'
+import AddTodo from './components/addTodo'
+import './App.css'
+
+
+function App() {
+
+
+  return (
+    <>
+      <AddTodo />
+      <Todos />
+    </>
+  )
+}
+
+export default App
